1e6bf2749605f7b5aec9b97e8bada577158168d4
JimmyD-Be <dom.jimmy@gmail.com>
2026-09-17 14:12:19 +0200

    Forms settings

--- web_data_v2/src/pages/settings.tsx
+++ web_data_v2/src/pages/settings.tsx
@@ -14,6 +14,7 @@ import { BackupSettings } from "../components/Settings/Backup";
 import { ImportSettings } from "../components/Settings/Import";
 import { RebootSettings } from "../components/Settings/Reboot";
 import { SoftwareUpdateSettings } from "../components/Settings/SoftwareUpdate";
+import { FirmwareUpdateSettings } from "../components/Settings/FirmwareUpdate";
 
 export function Settings() {
   return (
@@ -68,6 +69,7 @@ export function Settings() {
           <FirmwareSettings />
           <WebUISettings />
           <WebUIUpdateSettings />
+          <FirmwareUpdateSettings />
           <BackupSettings />
           <ImportSettings />
           <RebootSettings />
