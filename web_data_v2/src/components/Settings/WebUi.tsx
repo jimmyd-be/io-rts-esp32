21b24cdb747c508629d3115a40182fde8e637795
JimmyD-Be <dom.jimmy@gmail.com>
2026-09-17 14:55:12 +0200

    settings components

--- web_data_v2/src/components/Settings/WebUi.tsx
+++ web_data_v2/src/components/Settings/WebUi.tsx
@@ -1,11 +1,15 @@
-export function WebUISettings() {
+import { InfoResponse } from "../../models/Types";
+
+export function WebUISettings({data}: {data: InfoResponse | undefined}) {
   return (
     <div class="settings-row">
       <span class="row-label" data-i18n="settings.row.web-ui">
         Web UI
       </span>
       <div class="row-right">
-        <span class="row-value" id="web-version"></span>
+        <span class="row-value" id="web-version">
+          {data?.web_version}
+        </span>
       </div>
     </div>
   );
